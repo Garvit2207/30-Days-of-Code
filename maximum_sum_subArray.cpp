dbea5452f30b36173b4e551638fec2e7cf2718cc
Garvit2207 <garvitgarg1995@gmail.com>
2022-09-03 00:08:16 +0530

    Just Grinding

--- maximum_sum_subArray.cpp
+++ maximum_sum_subArray.cpp
@@ -12,20 +12,24 @@ for (int i = 0; i < n; i++)
     cin>>arr[i];
 }
 
-int max_sum = arr[0];
-int temp_sum= arr[0];
+int max_sum = 0;
+int temp_sum= 0;
 
-for (int i = 1; i < n; i++)
+for (int i = 0; i < n; i++)
 {
+if(arr[i]>temp_sum && temp_sum<0){
+       temp_sum = arr[i];
+    }
+    else{
+        temp_sum = temp_sum + arr[i];
+    }
+
     if(temp_sum>max_sum){
         max_sum = temp_sum;
     }
-   else if(arr[i]>temp_sum && temp_sum<0){
-temp_sum = arr[i];
-    }
+   
     
-    temp_sum = temp_sum + arr[i];
-
+ 
 }
 
 cout<<max_sum<<endl;
