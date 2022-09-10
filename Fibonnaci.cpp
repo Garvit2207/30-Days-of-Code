7f9970af361986926b84f229d0f55ec3645a70a0
Garvit2207 <garvitgarg1995@gmail.com>
2022-09-10 23:32:29 +0530

    Just Grinding

---
+++ Fibonnaci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n){
+
+if(n==0){
+    return 0;
+}
+
+if (n==1){
+    return 1;
+}
+
+int answer=fib(n-1) + fib(n-2);
+return answer;
+
+}
+
+
+int main(){
+
+int n;
+cin>>n;
+
+cout<<fib(n);
+    return 0;
+}
